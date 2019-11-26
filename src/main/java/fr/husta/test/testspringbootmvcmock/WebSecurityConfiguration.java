73482341082c862e18b2d169c20e63b9f32855e0
Guillaume HUSTA <guillaume.husta@gmail.com>
2019-11-26 11:51:51 +0100

    Add Spring Security

---
+++ src/main/java/fr/husta/test/testspringbootmvcmock/WebSecurityConfiguration.java
@@ -0,0 +1,34 @@
+package fr.husta.test.testspringbootmvcmock;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        PasswordEncoder encoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
+        auth.inMemoryAuthentication()
+                .passwordEncoder(encoder)
+                .withUser("spring")
+                .password(encoder.encode("secret"))
+                .roles("USER");
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .antMatchers("/api/public/**").permitAll()
+                .antMatchers("/api/private/**").authenticated()
+                .anyRequest().denyAll()
+                .and()
+                .httpBasic();
+    }
+
+}
