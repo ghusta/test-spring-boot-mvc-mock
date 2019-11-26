f796b32508f740c9c0311e5e6509a60d682f1ee8
Guillaume HUSTA <guillaume.husta@gmail.com>
2019-11-26 12:30:52 +0100

    Add /is-admin in /auth endpoint

--- src/main/java/fr/husta/test/testspringbootmvcmock/api/AuthResource.java
+++ src/main/java/fr/husta/test/testspringbootmvcmock/api/AuthResource.java
@@ -4,11 +4,15 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.annotation.CurrentSecurityContext;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Objects;
 
 @RestController
 @RequestMapping(path = "/auth")
@@ -20,6 +24,17 @@ public class AuthResource {
     public ResponseEntity<String> whoami(@AuthenticationPrincipal(errorOnInvalidType = true) UserDetails userDetails) {
         String login = userDetails.getUsername();
         return ResponseEntity.ok(login);
+    }
+
+    @GetMapping(path = "is-admin", produces = MediaType.APPLICATION_JSON_VALUE)
+    @PreAuthorize("isAuthenticated()")
+    public ResponseEntity<Boolean> isAdmin(@CurrentSecurityContext(expression = "authentication") Authentication authentication) {
+        boolean hasAdmin = authentication.getAuthorities()
+                .stream()
+                .filter(Objects::nonNull)
+                .anyMatch(ga -> ga.getAuthority().equalsIgnoreCase("ROLE_ADMIN"));
+
+        return ResponseEntity.ok(hasAdmin);
     }
 
 }
