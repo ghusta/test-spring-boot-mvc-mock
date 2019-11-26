4c8b38f198f2c20249ffeb0c4d4e80612be7f403
Guillaume HUSTA <guillaume.husta@gmail.com>
2019-11-26 17:23:31 +0100

    Make some tests pass
    See answer : https://stackoverflow.com/a/32911575/1677594

--- src/test/java/fr/husta/test/testspringbootmvcmock/api/PublicResourceTest.java
+++ src/test/java/fr/husta/test/testspringbootmvcmock/api/PublicResourceTest.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.web.servlet.DispatcherServletPath;
+import org.springframework.boot.autoconfigure.web.servlet.WebMvcProperties;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.core.env.Environment;
 import org.springframework.http.MediaType;
@@ -13,10 +14,13 @@ import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.fail;
 import static org.springframework.security.test.web.servlet.response.SecurityMockMvcResultMatchers.authenticated;
+import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
@@ -32,9 +36,12 @@ class PublicResourceTest {
     private Environment env;
 
     @Autowired
-    private WebApplicationContext webApplicationContext;
+    private WebMvcProperties webMvcProperties;
 
     @Autowired
+    private WebApplicationContext webApplicationContext;
+
+    //     @Autowired
     private MockMvc mockMvc;
 
     /**
@@ -42,6 +49,8 @@ class PublicResourceTest {
      */
     @Autowired
     private DispatcherServletPath dispatcherServletPath;
+
+    private WebMvcProperties.Servlet webMvcServlet;
 
     @BeforeEach
     void setUp() {
@@ -51,15 +60,61 @@ class PublicResourceTest {
                 dispatcherServletPath.getPath());
         log.debug("Auto-config, computed from Path : Prefix = {}; ServletUrlMapping = {}",
                 dispatcherServletPath.getPrefix(), dispatcherServletPath.getServletUrlMapping());
+
+        webMvcServlet = webMvcProperties.getServlet();
+
+        // See answer : https://stackoverflow.com/a/32911575/1677594
+        mockMvc = MockMvcBuilders
+                .webAppContextSetup(webApplicationContext)
+                .defaultRequest(get("/").servletPath(dispatcherServletPath.getPath()))
+                .apply(springSecurity())
+                .build();
     }
 
     @Test
     void shouldGetGreetingsReturnOk() throws Exception {
+        String springSecurityPattern = webMvcServlet.getPath("/public/**");
+        log.debug("Pattern permitAll = {}", springSecurityPattern);
+        String processedPath = webMvcServlet.getPath("/public/greetings");
+        log.debug("URL = {}", processedPath);
+        MvcResult mvcResult = mockMvc.perform(get(processedPath).accept(MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$").value("Hello !"))
+                .andReturn();
+
+        // Just to use AssertJ
+        assertThat(mvcResult).isNotNull();
+    }
+
+    @Test
+    void shouldGetGreetingsReturnOkWithServletPath() throws Exception {
+        if (webMvcServlet.getPath().equals("/")) {
+            fail("NOT AVAILABLE");
+        }
+
+        // Try .servletPath("/prefix") (https://docs.spring.io/spring/docs/current/spring-framework-reference/testing.html#spring-mvc-test-server-performing-requests)
+        MvcResult mvcResult = mockMvc.perform(
+//                get(webMvcServlet.getPath("/public/greetings")).servletPath(webMvcServlet.getPath()).accept(MediaType.APPLICATION_JSON))
+                get("/public/greetings").servletPath(webMvcServlet.getPath()).accept(MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$").value("Hello !"))
+                .andReturn();
+
+        // Just to use AssertJ
+        assertThat(mvcResult).isNotNull();
+    }
+
+    @Test
+    void shouldGetGreetingsReturnOkWithPathUnprocessed() throws Exception {
         MvcResult mvcResult = mockMvc.perform(get("/public/greetings").accept(MediaType.APPLICATION_JSON))
                 .andDo(print())
                 .andExpect(status().isOk())
-                .andExpect(jsonPath("$").value("Hello !"))
                 .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$").value("Hello !"))
                 .andReturn();
 
         // Just to use AssertJ
@@ -90,7 +145,7 @@ class PublicResourceTest {
     @Test
     @WithMockUser(username = "spring")
     void shouldGetGreetingsReturnOkBeingAuthenticated() throws Exception {
-        mockMvc.perform(get("/public/greetings").accept(MediaType.APPLICATION_JSON))
+        mockMvc.perform(get(webMvcServlet.getPath("/public/greetings")).accept(MediaType.APPLICATION_JSON))
                 .andDo(print())
                 .andExpect(authenticated().withUsername("spring"))
                 .andExpect(status().isOk())
