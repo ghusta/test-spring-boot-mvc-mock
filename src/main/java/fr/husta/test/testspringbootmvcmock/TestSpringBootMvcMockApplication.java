ef60c86d2291023ea5acb9bc0a55634543497aad
Guillaume HUSTA <guillaume.husta@gmail.com>
2019-11-26 10:11:19 +0100

    Initial commit

---
+++ src/main/java/fr/husta/test/testspringbootmvcmock/TestSpringBootMvcMockApplication.java
@@ -0,0 +1,13 @@
+package fr.husta.test.testspringbootmvcmock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestSpringBootMvcMockApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestSpringBootMvcMockApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fr/husta/test/testspringbootmvcmock/TestSpringBootMvcMockApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.husta.test.testspringbootmvcmock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestSpringBootMvcMockApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
